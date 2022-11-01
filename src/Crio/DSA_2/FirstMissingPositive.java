67cc01bbb7d7e475f8b09347f6a773c7217a19e4
Riyaz <sayyadriyaz0414@gmail.com>
2022-11-01 14:42:45 +0530

    set diagonal zero

--- src/Crio/DSA_2/FirstMissingPositive.java
+++ src/Crio/DSA_2/FirstMissingPositive.java
@@ -4,7 +4,25 @@ import java.util.*;
 
 public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
-        return 0;
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int a=0;
+        int b=0;
+        if(nums[0]>2)
+            return 1;
+        for(int i=0;i< nums.length;i++){
+            if ((i+1)<nums.length) {
+                if(nums[i]>=0) {
+                    a = nums[i]+1;
+                    b = nums[i + 1];
+                    System.out.println(a+" "+b);
+                    if(a==b) continue;
+                    else return (a);
+                }
+            }
+        }
+
+        return b+1;
     }
 
     public static void main(String[] args) {
---
+++ src/Crio/DSA_2/SetMatrixZeroes.java
@@ -0,0 +1,35 @@
+package Crio.DSA_2;
+
+import java.io.*;
+import java.util.*;
+
+
+public class SetMatrixZeroes {
+    public void setMatrixZeroes(int[][] matrix) {
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for(int i = 0 ; i < m ; ++i) {
+            for(int j = 0 ; j < n ; ++j) {
+                matrix[i][j] = in.nextInt();
+            }
+        }
+
+        in.close();
+        new SetMatrixZeroes().setMatrixZeroes(matrix);
+
+        for(int i = 0 ; i < m ; ++i) {
+            for(int j = 0 ; j < n ; ++j) {
+                System.out.print(matrix[i][j]);
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+}
