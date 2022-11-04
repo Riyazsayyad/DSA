f90496282eba9f8e89464a9c1711728e25cff423
Riyaz <sayyadriyaz0414@gmail.com>
2022-11-04 17:49:43 +0530

    false

---
+++ src/Crio/DSA_2/MaxSumTriplet.java
@@ -0,0 +1,29 @@
+package Crio.DSA_2;
+import java.util.*;
+
+public class MaxSumTriplet {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            long arr[] = new long[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextLong();
+
+            long result = maxSumTriplet(n, arr);
+
+            System.out.println(result);
+
+        }
+
+    }
+
+    static long maxSumTriplet(int n, long arr[]) {
+        Arrays.sort(arr);
+        long sum = arr[n-1]+arr[n-2]+arr[n-3];
+        return sum;
+    }
+}
