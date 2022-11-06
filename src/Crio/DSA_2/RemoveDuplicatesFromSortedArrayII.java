a7cdf37e0a758de92865f1dad0de637d962ce6c0
Riyaz <sayyadriyaz0414@gmail.com>
2022-11-06 10:11:05 +0530

    ____

--- src/Crio/DSA_2/RemoveDuplicatesFromSortedArrayII.java
+++ src/Crio/DSA_2/RemoveDuplicatesFromSortedArrayII.java
@@ -20,34 +20,12 @@ public class RemoveDuplicatesFromSortedArrayII {
             System.out.print(arr[i] + " ");
 
     }
-//    int removeDuplicates(vector<int>& nums) {
-//        int i = 0;
-//        for (int n : nums)
-//            if (i < 2 || n > nums[i-2])
-//                nums[i++] = n;
-//        return i;
-//    }
+
     static int removeDuplicatesFromSortedArrayII(int n, int[] arr) {
         int i=0;
         for(int a:arr){
             if(i<2 || a > arr[i-2]) arr[i++]=a;
         }
         return i;
-        /*
-        ArrayList<Integer> aL = new ArrayList<Integer>();
-        for(int i=0;i<n-1;i++){
-            int cnt =0;
-            aL.add(arr[i]);
-            while (arr[i]==arr[i+1] && i<n){
-                i++;
-                cnt++;
-                aL.add(arr[i]);
-                if (cnt>1)  arr[i]=0;
-            }cnt=0;aL.add(arr[i]);
-
-        }
-        System.out.println(Arrays.asList(aL));
-        return arr.length;
-        */
     }
 }
