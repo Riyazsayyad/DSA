38e091d7243e390fb426739cdde5ccd9678b444e
Riyaz <sayyadriyaz0414@gmail.com>
2022-11-08 18:55:48 +0530

    meeting room yet to be optimised, kaden algorithm learned )

---
+++ src/Crio/DSA_2/ContigiousSequence.java
@@ -0,0 +1,30 @@
+package Crio.DSA_2;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ContigiousSequence {
+    public static  long contigiousSequence(int arr[] , int n) {
+
+        long cs=0,ms=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            cs+=arr[i];
+            if(cs>ms)    ms=cs;
+            if(cs<0)    cs=0;
+        }
+        return ms;
+    }
+    public static void  main (String args []) {
+
+        Scanner sc =  new  Scanner (System.in);
+
+        int n = sc.nextInt();
+        int arr [] = new int[n];
+        for ( int i = 0 ; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        long max = contigiousSequence(arr, n);
+        System.out.println(max);
+
+    }
+}
