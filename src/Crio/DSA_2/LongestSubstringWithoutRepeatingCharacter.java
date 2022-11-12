471cccc278f94dd3348a57f3aeb0b74e35f6813c
Riyaz <sayyadriyaz0414@gmail.com>
2022-11-12 20:29:00 +0530

    Period.

--- src/Crio/DSA_2/LongestSubstringWithoutRepeatingCharacter.java
+++ src/Crio/DSA_2/LongestSubstringWithoutRepeatingCharacter.java
@@ -4,19 +4,18 @@ import java.util.*;
 public class LongestSubstringWithoutRepeatingCharacter {
     public int lengthOfLongestSubstring(String s) {
         int high=0,low=0,max =0;
-        Set<Character> set = new HashSet<>();
         int[] arr = new int[256];
-        //aabcccbcb
+        Arrays.fill(arr,-1);
+        if(s.length()==0) return 0;
+        if(s.length()==1) return 1;
         for(high=0,low=0;high<s.length();high++){
-            if(!set.contains(s.charAt(high))){
-                set.add(s.charAt(high));
-                high++;
-            }else{
-                set.remove(s.charAt(low));
-                low++;
-            }
-            max = Math.max(max,set.size());
+            low = Math.max(low,arr[s.charAt(high)]);
+            System.out.print(low+"L "+high+"H "+s.charAt(low)+"\t\n");
+            max = Math.max(max,high-low);
+            arr[s.charAt(high)] = high;
+            System.out.print(arr[s.charAt(high)]+"\t\n");
         }
+        if(max == s.length()-1) return ++max;
         return max;
     }
     public static void main(String[] args) {
