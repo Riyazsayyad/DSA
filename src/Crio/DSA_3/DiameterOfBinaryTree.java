ddca02030f6ae274d29bdb3317394be5e109f780
Riyaz <sayyadriyaz0414@gmail.com>
2022-12-13 23:57:43 +0530

    Tree Started :

--- src/Crio/DSA_3/DiameterOfBinaryTree.java
+++ src/Crio/DSA_3/DiameterOfBinaryTree.java
@@ -1,4 +1,20 @@
 package Crio.DSA_3;
 
 public class DiameterOfBinaryTree {
+    public int maxDiameter = 0;
+    public int getDiameter(TreeNode root){
+        if(root == null) return 0;
+
+        int left = getDiameter(root.left);
+        int right = getDiameter(root.right);
+
+        maxDiameter = Math.max(maxDiameter,left + right);
+
+        return Math.max(left,right)+1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        getDiameter(root);
+        return maxDiameter;
+    }
 }
