24dd5010027290f0b45baafce7d5afed156aeebe
Riyaz Sayyad <48363327+Riyazsayyad@users.noreply.github.com>
2022-12-20 11:45:47 +0530

    Update SortLinkedList.java
    updated code LL sort

--- src/Crio/DSA_3/SortLinkedList.java
+++ src/Crio/DSA_3/SortLinkedList.java
@@ -2,44 +2,56 @@ package Crio.DSA_3;
 
 public class SortLinkedList {
     public ListNode sortList(ListNode head) {
-        mergeSort(head);
-        return head;
-    }
-
-    public void mergeSort(ListNode head) {
-        ListNode tHead = head;
-        if(tHead == null || tHead.next == null) return;
-        ListNode a = null, b =null;
-        splitLL(tHead,a,b);
-        mergeSort(a);
-        mergeSort(b);
         
-        head = sortedMerge(a,b);
+        if(head == null || head.next == null)   return head;
+
+        ListNode mid = getMid(head);
+        ListNode midNext = mid.next;
+        mid.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(midNext);
+
+        return merge(left,right);
     }
 
-    public ListNode sortedMerge(ListNode a, ListNode b) {
-        ListNode res = null;
-        if(a == null) return b;
-        if(b == null) return a;
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL;
 
-        if(a.val <= b.val){
-            res = a;
-            a.next = sortedMerge(a.next,b);
-        }else {
-            res = b;
-            b.next = sortedMerge(a,b.next);
+        while (left != null && right != null){
+            if(left.val < right.val){
+                temp.next = left;
+                left = left.next;
+            }
+            else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
         }
-        return res;
+
+        while (left != null) {
+            temp.next = left;
+            left = left.next;
+            temp = temp.next;
+        }
+
+        while (right != null) {
+            temp.next = right;
+            right = right.next;
+            temp = temp.next;
+        }
+
+        return mergedLL.next;
     }
 
-    public void splitLL(ListNode node,ListNode a, ListNode b){
-        ListNode slow =node, fast =node;
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
         while (fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        a = node;
-        b = slow.next;
-        slow.next = null;
+        return slow;
     }
 }
