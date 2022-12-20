a5b5cf2a9d96762a6efbad095b27face21341aa5
Riyaz <sayyadriyaz0414@gmail.com>
2022-12-20 20:26:39 +0530

    detect and remove cycle debugged

--- src/Crio/DSA_3/DetectAndRemoveCycle.java
+++ src/Crio/DSA_3/DetectAndRemoveCycle.java
@@ -1,7 +1,7 @@
 package Crio.DSA_3;
 
 public class DetectAndRemoveCycle {
-  public boolean detectAndRemoveCycle(ListNode head){
+    public boolean detectAndRemoveCycle(ListNode head){
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null){
             fast = fast.next.next;
@@ -10,28 +10,39 @@ public class DetectAndRemoveCycle {
         }
         return false;
     }
-
+    // 1 > 2 > 3 > 4L > 6 > 7L
+    // here slow and fast meet at node with val 4 ( loop between 4 - 7)
     private boolean removeCycle(ListNode head, ListNode fast, ListNode slow) {
-        int count =1;
-        while (true){
+        int count = 1;
+        // finding length count between two loop points
+        do {
             slow = slow.next;
             count++;
-            if(fast == slow)    break; 
-        }
-        //System.out.println(count);
-        ListNode temp = head;
-        while (temp != slow) {
-            count++;
-            temp = temp.next;
-        }
-        //System.out.println(count);
-        temp = head;
-        while (count >= 0){
-            temp = temp.next;
-            count--;
-        }
-        temp.next =null;
+        } while (fast != slow.next);
 
+        // incrementing tHead to next node by the count of nodes
+        // in our example count will be 3
+        ListNode tHead = head;
+        while (count > 0){
+            --count;
+            tHead = tHead.next;
+        }
+
+        // for test 1L > 2L
+        if(tHead == head){
+            while (tHead.next != head) tHead = tHead.next;
+            tHead.next = null;
+            return true;
+        }
+
+        // our Thead will be on node val 3
+        while (tHead.next != head.next){
+            head = head.next;
+            tHead = tHead.next;
+        }
+
+
+        tHead.next = null;
         return true;
     }
 }
