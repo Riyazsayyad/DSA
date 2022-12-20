b27cbc90486e56a702508a91a6bccc36c9c7adb1
Riyaz <sayyadriyaz0414@gmail.com>
2022-12-20 13:46:57 +0530

    Merger Conflict Resolved

--- src/Crio/DSA_3/SortLinkedList.java
+++ src/Crio/DSA_3/SortLinkedList.java
@@ -2,44 +2,7 @@ package Crio.DSA_3;
 
 public class SortLinkedList {
     public ListNode sortList(ListNode head) {
-<<<<<<< HEAD
-        if(head == null || head.next == null) return head;
 
-        ListNode mid = getMid(head);
-        ListNode midNext = mid.next;
-
-        mid.next = null;
-
-        ListNode left = sortList(head);
-        ListNode right = sortList(midNext);
-
-        return sortedMerge(left,right);
-    }
-
-    private ListNode getMid(ListNode head) {
-        if(head == null) return head;
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        return slow;
-    }
-
-    private ListNode sortedMerge(ListNode left, ListNode right) {
-        ListNode res = null;
-        if(left == null)    return right;
-        if(right == null)    return left;
-
-        if(left.val <= right.val){
-            res = left;
-            res.next = sortedMerge(left.next,right);
-        }else {
-            res = right;
-            res.next = sortedMerge(left,right.next);
-=======
-        
         if(head == null || head.next == null)   return head;
 
         ListNode mid = getMid(head);
@@ -65,7 +28,6 @@ public class SortLinkedList {
                 right = right.next;
             }
             temp = temp.next;
->>>>>>> 1a64942ba5e61439a965c76b54926fd875d80761
         }
 
         while (left != null) {
@@ -83,10 +45,6 @@ public class SortLinkedList {
         return mergedLL.next;
     }
 
-<<<<<<< HEAD
-
-
-=======
     private ListNode getMid(ListNode head) {
         ListNode slow = head;
         ListNode fast = head.next;
@@ -96,5 +54,4 @@ public class SortLinkedList {
         }
         return slow;
     }
->>>>>>> 1a64942ba5e61439a965c76b54926fd875d80761
 }
