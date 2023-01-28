bf753d57012718e6e75695cc6aa458c8bbf30fd1
Riyaz <sayyadriyaz0414@gmail.com>
2023-01-29 01:03:09 +0530

    Blank

--- src/Crio/DSA_4/Graph_BFS_DFS/WallsAndGates.java
+++ src/Crio/DSA_4/Graph_BFS_DFS/WallsAndGates.java
@@ -2,36 +2,50 @@ package Crio.DSA_4.Graph_BFS_DFS;
 import java.util.*;
 import java.io.*;
 class WallsAndGates {
-
-
-    private static int INF = Integer.MAX_VALUE;
+    static boolean[][] visited;
+    static int INF = Integer.MAX_VALUE;
     public static int[][] WallsAndGates(int [][] grid , int n, int m) {
-        Queue<int[]> Q = new LinkedList<>();
+        //m and n are not absolute dimensions of the grid
+        int[][] result = grid;
+        visited = new boolean[n+1][m+1];
+        Queue<GatePair> Q = new LinkedList<>();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(grid[i][j] == 0){
-                    Q.offer(new int[]{i,j});
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(grid[i][j] == 0) Q.offer(new GatePair(i,j));
             }
         }
 
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
+        int[] X = {0,0,-1,1};
+        int[] Y = {1,-1,0,0};
 
-        while (!Q.isEmpty()) {
-            int[] curr = Q.poll();
-            int x = curr[0], y = curr[1];
+        while (!Q.isEmpty()){
+            GatePair obj = Q.poll();
+            int row = obj.row,col = obj.col;
             for (int i = 0; i < 4; i++) {
-                int newX = x + dx[i], newY = y + dy[i];
-                if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == INF && grid[newX][newY] != -1) {
-                    grid[newX][newY] = grid[x][y] + 1;
-                    Q.offer(new int[] {newX, newY});
+                int nR = row + X[i],nC = col + Y[i];
+                if(isValid(nR,nC,m,n,grid)){
+                    visited[nR][nC] = true;
+                    result[nR][nC] = result[row][col] + 1;
+                    Q.offer(new GatePair(nR,nC));
                 }
             }
         }
-        return grid;
+        return result;
     }
+
+    private static boolean isValid(int row, int col,int rowL, int colL,int[][] grid) {
+        return row > -1 && col > -1 && row < rowL && col < colL && !visited[row][col] && grid[row][col] == INF;
+    }
+
+    private static class GatePair{
+        int row,col;
+        private GatePair(int row,int col){
+            this.col = col;
+            this.row = row;
+        }
+    }
+
 
     public static void main(String []args) {
         int n, m;
@@ -53,44 +67,3 @@ class WallsAndGates {
         }
     }
 }
-
-
-/*
-    private static int findGate(int[][] grid, int row, int col) {
-        Queue<GatePair> Q = new LinkedList<>();
-        Q.offer(new GatePair(row,col));
-        int ans = 0;
-        int rowL = grid.length,colL = grid[0].length;
-        while (!Q.isEmpty()){
-            ans++;
-            GatePair obj = Q.poll();
-            int r = obj.row,c = obj.col;
-            if(grid[r][c] == 0) return ans;
-            if(isValid(r+1,c,rowL,colL,grid)){
-                Q.offer(new GatePair(r+1,c));
-            }
-            if(isValid(r,c+1,rowL,colL,grid)){
-                Q.offer(new GatePair(r,c+1));
-            }
-            if(isValid(r-1,c,rowL,colL,grid)){
-                Q.offer(new GatePair(r-1,c));
-            }
-            if(isValid(r,c-1,rowL,colL,grid)){
-                Q.offer(new GatePair(r,c-1));
-            }
-        }
-        return Integer.MAX_VALUE;
-    }
-
-    private static boolean isValid(int row, int col,int rowL, int colL,int[][] grid) {
-        return row > -1 && col > -1 && row < rowL && col < colL && !visited[row][col] && grid[row][col] != -1;
-    }
-
-    private static class GatePair{
-        int row,col;
-        private GatePair(int row,int col){
-            this.col = col;
-            this.row = row;
-        }
-    }
-*/
